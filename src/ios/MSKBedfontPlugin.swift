62a860ad7c4ae667afdda5372bb7a039732a4075
Shishir Shetty <shettys@mskcc.org>
2023-10-03 19:52:00 -0400

    Comment commandDelegate

--- src/ios/MSKBedfontPlugin.swift
+++ src/ios/MSKBedfontPlugin.swift
@@ -272,7 +272,7 @@ class MSKBedfontPlugin: CDVPlugin {
             // Print the JSON data as a string (for demonstration purposes)
             if let jsonString = String(data: jsonData, encoding: .utf8) {
                 let jsCode = "cordova.fireDocumentEvent('\(eventName)', \(jsonString));"
-                commandDelegate.evalJs(jsCode)
+                //commandDelegate.evalJs(jsCode)
             }
         } catch {
             
