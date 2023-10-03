94dcf3e51cc526aafae56a24d4ba346fd52bcf4f
Shishir Shetty <shettys@mskcc.org>
2023-10-03 18:03:39 -0400

    Global object for commandDelegate

--- src/ios/MSKBedfontPlugin.swift
+++ src/ios/MSKBedfontPlugin.swift
@@ -5,7 +5,6 @@
 //
 import Foundation
 import SmokerlyzerSDK
-import Cordova
 
 
 
@@ -56,6 +55,14 @@ class MSKBedfontPlugin: CDVPlugin {
     
     var isEnabled: Bool = false
     
+    private var myCommandDelegate: CDVCommandDelegate?
+
+    override func pluginInitialize() {
+        super.pluginInitialize()
+
+        // Initialize myCommandDelegate with self.commandDelegate during plugin initialization
+        myCommandDelegate = self.commandDelegate
+    }
     
     @objc(initialize:)func initialize(command : CDVInvokedUrlCommand) {
         
@@ -79,42 +86,42 @@ class MSKBedfontPlugin: CDVPlugin {
     
     @objc(performScanning:)func performScanning(command : CDVInvokedUrlCommand) {
         
-        self.perform_Scanning()
+        self.perform_Scanning(commandDelegate: self.commandDelegate)
         let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(result, callbackId: command.callbackId)
     }
     
     @objc(performDisconnection:)func performDisconnection(command : CDVInvokedUrlCommand) {
         
-        self.perform_Disconnection()
+        self.perform_Disconnection(commandDelegate: self.commandDelegate)
         let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(result, callbackId: command.callbackId)
     }
     
     @objc(testNorecovery:)func testNorecovery(command : CDVInvokedUrlCommand) {
         
-        self.perform_TestNorecovery()
+        self.perform_TestNorecovery(commandDelegate: self.commandDelegate)
         let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(result, callbackId: command.callbackId)
     }
     
     @objc(getDeviceFirmware:)func getDeviceFirmware(command : CDVInvokedUrlCommand) {
         
-        self.get_DeviceFirmware()
+        self.get_DeviceFirmware(commandDelegate: self.commandDelegate)
         let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(result, callbackId: command.callbackId)
     }
     
     @objc(getDeviceSerialNumber:)func getDeviceSerialNumber(command : CDVInvokedUrlCommand) {
         
-        self.get_DeviceSerialNumber()
+        self.get_DeviceSerialNumber(commandDelegate: self.commandDelegate)
         let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
         self.commandDelegate.send(result, callbackId: command.callbackId)
     }
     
     
     
-    func perform_Scanning() {
+    func perform_Scanning(commandDelegate: CDVCommandDelegate) {
         if smokerlyzerBluetooth != nil {
             let didScanStart = smokerlyzerBluetooth.scanForPeripheral(
                 stopOnFirstResult: false,
@@ -123,15 +130,15 @@ class MSKBedfontPlugin: CDVPlugin {
                 onStopped: {_, error in
                     if let error = error {
                         //self.log(message: "Scan error: " + error.localizedDescription)
-                        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: false)
+                        self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: false)
                     } else {
-                        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: false)
+                        self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: false)
                         //self.log(message: "Scan stopped.")
                     }
                 }
             )
             if didScanStart {
-                self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: true)
+                self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_SCANSTATECHANGE, boolval: true)
                 //self.log(message: "Scan was allowed to start.")
                 self.performConnect()
             }
@@ -144,19 +151,19 @@ class MSKBedfontPlugin: CDVPlugin {
         let scanStarted = smokerlyzerBluetooth.scanForPeripheral(
             onDiscovery: { scan, _ in
                 //self.log(message: "Peripheral found, connecting...")
-                self.sendScanningEvents(connectResult: "ZEROING", logMessage: "Zeroing the sensor, please wait...")
+                self.sendScanningEvents(commandDelegate: self.commandDelegate, connectResult: "ZEROING", logMessage: "Zeroing the sensor, please wait...")
                 smokerlyzerBluetooth.connectToPeripheral(peripheral: scan.peripheralIdentifier, connected: {result in
                     switch result {
                     case .success(let peripheralId):
                         //self.log(message: "Successfully connected to " + peripheralId.name)
                         self.isConnectedText = "Connected"
-                        self.sendScanningEvents(connectResult: "SUCCESS", logMessage: "Successfully connected to " + peripheralId.name)
-                        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: true)
+                        self.sendScanningEvents(commandDelegate: self.commandDelegate, connectResult: "SUCCESS", logMessage: "Successfully connected to " + peripheralId.name)
+                        self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: true)
                     case .failure(let error):
                         //self.log(message: "Failed to connect with error: \(error.localizedDescription)")
                         self.isConnectedText = "Disconnected"
-                        self.sendScanningEvents(connectResult: "ERROR_FAILED_TO_CONNECT", logMessage: "Failed to connect with error: \(error.localizedDescription)")
-                        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
+                        self.sendScanningEvents(commandDelegate: self.commandDelegate, connectResult: "ERROR_FAILED_TO_CONNECT", logMessage: "Failed to connect with error: \(error.localizedDescription)")
+                        self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
                     }
                 })
             },
@@ -170,92 +177,92 @@ class MSKBedfontPlugin: CDVPlugin {
         
     }
     
-    func perform_Disconnection() {
-        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
+    func perform_Disconnection(commandDelegate: CDVCommandDelegate) {
+        self.sendStateChangeEvents(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
         self.isEnabled = false
         if smokerlyzerBluetooth != nil {
             smokerlyzerBluetooth.disconnectFromPeripheral()
         }
     }
     
-    func perform_TestNorecovery() {
+    func perform_TestNorecovery(commandDelegate: CDVCommandDelegate) {
         if smokerlyzerBluetooth != nil {
             smokerlyzerBluetooth.getCoppm(callback: {result in
                 switch result {
                 case .success(let ppm):
                     //self.log(message: "PPM value is " + String(ppm.reading))
-                    self.sendScanningResults(statusName: "PPM value is " + String(ppm.reading), ppm: ppm.reading, isSuccessful: true)
+                    self.sendScanningResults(commandDelegate: self.commandDelegate, statusName: "PPM value is " + String(ppm.reading), ppm: ppm.reading, isSuccessful: true)
                 case .failure(let error):
                     //self.log(message: "Error: " + error.localizedDescription)
-                    self.sendScanningResults(statusName: "Error: " + error.localizedDescription, ppm: -1, isSuccessful: false)
+                    self.sendScanningResults(commandDelegate: self.commandDelegate, statusName: "Error: " + error.localizedDescription, ppm: -1, isSuccessful: false)
                 }
             })
         }
         
     }
-    func get_DeviceFirmware() {
+    func get_DeviceFirmware(commandDelegate: CDVCommandDelegate) {
         if smokerlyzerBluetooth != nil {
             smokerlyzerBluetooth.getFirmwareVersion(callback: { result in
                 switch result {
                 case .success(let firmware):
                     //self.log(message: "Firmware version: " + firmware.version)
-                    self.sendDeviceDetails(eventName: self.BEDFONT_EVENT_DEVICE_FIRMWARE, deviceParam: firmware.version)
+                    self.sendDeviceDetails(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_DEVICE_FIRMWARE, deviceParam: firmware.version)
                 case .failure(let error):
                     //self.log(message: "Error: " + error.localizedDescription)
-                    self.sendDeviceDetails(eventName: self.BEDFONT_EVENT_DEVICE_FIRMWARE, deviceParam: "Error: " + error.localizedDescription)
+                    self.sendDeviceDetails(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_DEVICE_FIRMWARE, deviceParam: "Error: " + error.localizedDescription)
                 }
             })
         }
     }
     
-    func get_DeviceSerialNumber() {
+    func get_DeviceSerialNumber(commandDelegate: CDVCommandDelegate) {
         if smokerlyzerBluetooth != nil {
             smokerlyzerBluetooth.getSerial(callback: { result in
                 switch result {
                 case .success(let serial):
                     //self.log(message: "Serial number: " + serial.serial)
-                    self.sendDeviceDetails(eventName: self.BEDFONT_EVENT_DEVICE_SERIALNUMBER, deviceParam: serial.serial)
+                    self.sendDeviceDetails(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_DEVICE_SERIALNUMBER, deviceParam: serial.serial)
                 case .failure(let error):
                     //self.log(message: "Error: " + error.localizedDescription)
-                    self.sendDeviceDetails(eventName: self.BEDFONT_EVENT_DEVICE_SERIALNUMBER, deviceParam: "Error: " + error.localizedDescription)
+                    self.sendDeviceDetails(commandDelegate: self.commandDelegate, eventName: self.BEDFONT_EVENT_DEVICE_SERIALNUMBER, deviceParam: "Error: " + error.localizedDescription)
                 }
             })
         }
     }
     
-    func sendStateChangeEvents(eventName: String, boolval: Bool) {
+    func sendStateChangeEvents(commandDelegate: CDVCommandDelegate, eventName: String, boolval: Bool) {
         let eventData: [String: Any] = [
             "changeState": boolval
         ]
-        self.fireEvent(eventName: eventName, eventData: eventData )
+        self.fireEvent(commandDelegate: CDVCommandDelegate, eventName: eventName, eventData: eventData )
     }
     
-    func sendScanningEvents(connectResult: String, logMessage: String) {
+    func sendScanningEvents(commandDelegate: CDVCommandDelegate,connectResult: String, logMessage: String) {
         let eventData: [String: Any] = [
             "connectResult": connectResult,
             "logMessage": logMessage
         ]
-        self.fireEvent(eventName: self.BEDFONT_EVENT_SCAN_RESULT, eventData: eventData)
+        self.fireEvent(commandDelegate: CDVCommandDelegate, eventName: self.BEDFONT_EVENT_SCAN_RESULT, eventData: eventData)
     }
     
-    func sendScanningResults(statusName: String, ppm: Int, isSuccessful: Bool) {
+    func sendScanningResults(commandDelegate: CDVCommandDelegate, statusName: String, ppm: Int, isSuccessful: Bool) {
         let eventData: [String: Any] = [
             "statusName": statusName,
             "ppm": ppm,
             "isSuccessful": isSuccessful
         ]
-        self.fireEvent(eventName: self.BEDFONT_EVENT_CONNECT_RESULT, eventData: eventData)
+        self.fireEvent(commandDelegate: CDVCommandDelegate, eventName: self.BEDFONT_EVENT_CONNECT_RESULT, eventData: eventData)
     }
     
-    func sendDeviceDetails(eventName: String, deviceParam: String) {
+    func sendDeviceDetails(commandDelegate: CDVCommandDelegate, eventName: String, deviceParam: String) {
         let eventData: [String: Any] = [
             "deviceDetail": deviceParam
         ]
         
-        self.fireEvent(eventName: eventName, eventData: eventData)
+        self.fireEvent(commandDelegate: CDVCommandDelegate, eventName: eventName, eventData: eventData)
     }
     
-    func fireEvent(eventName: String, eventData: [String: Any]) {
+    func fireEvent(commandDelegate: CDVCommandDelegate, eventName: String, eventData: [String: Any]) {
         
         do {
             // Convert the dictionary to JSON data
@@ -264,7 +271,7 @@ class MSKBedfontPlugin: CDVPlugin {
             // Print the JSON data as a string (for demonstration purposes)
             if let jsonString = String(data: jsonData, encoding: .utf8) {
                 let jsCode = "cordova.fireDocumentEvent('\(eventName)', \(jsonString));"
-                self.commandDelegate!.evalJs(jsCode)
+                commandDelegate!.evalJs(jsCode)
             }
         } catch {
             
@@ -283,12 +290,12 @@ extension MSKBedfontPlugin: ConnectionObserver {
         //log(message: "[connection observer] detected connect event")
         sensor = peripheral
         isConnectedText = "Connected"
-        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: true)
+        self.sendStateChangeEvents(commandDelegate: myCommandDelegate, eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: true)
     }
 
     func disconnected(from peripheral: PeripheralIdentifier) {
         //log(message: "[connection observer] detected disconnect event")
         isConnectedText = "Disconnected"
-        self.sendStateChangeEvents(eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
+        self.sendStateChangeEvents(commandDelegate: myCommandDelegate, eventName: self.BEDFONT_EVENT_BUTTONNAMECHANGE, boolval: false)
     }
 }
