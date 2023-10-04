956f7e1ce10b4ab93ad4a200d9348172c1911cc0
Shishir Shetty <shettys@mskcc.org>
2023-10-03 22:26:08 -0400

    Bug fix

--- src/ios/MSKBedfontPlugin.swift
+++ src/ios/MSKBedfontPlugin.swift
@@ -272,7 +272,7 @@ class MSKBedfontPlugin: CDVPlugin {
             // Print the JSON data as a string (for demonstration purposes)
             if let jsonString = String(data: jsonData, encoding: .utf8) {
                 //let jsCode = "cordova.fireDocumentEvent('OnButtonNameChangeEvent', \(jsonString)"
-                let jsCode = "cordova.fireDocumentEvent('" + eventName + "',"+ jsonString + ")"
+                let jsCode = "cordova.fireDocumentEvent('" + eventName + "'," + jsonString + ")"
                 commandDelegate.evalJs(jsCode)
             }
         } catch {
